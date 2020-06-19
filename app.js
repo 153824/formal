71e1ce91630108abeb8c024e10da53d682c73b44
WEID <chen@weid.ink>
2020-06-19 13:44:48 +0800

    fix: app.js下删除不必要的host字段

--- app.js
+++ app.js
@@ -27,13 +27,8 @@ App({
   isIos: false,
   qiniuUpload: qiniuUpload,
   isIphoneX: false,
-  // host: "https://api.dev.luoke101.com/hola/",
-  // host: "https://luoke.ampmfit.net/hola/", //请求host
-  host: "https://h5.luoke101.com/hola/",
-  host1: "https://admin.luoke101.com/hola/", //请求host——测试
-  host2: "http://localhost:3000/hola/", //请求host——测试
-  host3: "https://api.dev.luoke101.com/hola/", //微信支付——测试
-  host4: "https://h5.luoke101.com/haola/",
+  // host: "https://api.dev.luoke101.com",
+  host: "https://h5.luoke101.com",
   onLaunch: function(options) {
     var referrerInfo = options.referrerInfo;
     var menuBtnObj = wx.getMenuButtonBoundingClientRect();
@@ -304,10 +299,10 @@ App({
   doAjax: function(params) {
     //request请求
     var that = this;
-    var url = that.host + params.url;
+    var url = that.host + "/hola/" + params.url;
     var urlArr = ["homePages","positionTags","reports","buyTickets"];
     if( urlArr.includes(params.url.split("/")[0]) ){
-      url = that.host4 +params.url;
+      url = that.host+ "/haola/" +params.url;
     }
     if (!params.noLoading) {
       //默认显示加载中弹窗
@@ -319,7 +314,6 @@ App({
     params.data["userId"] = (that.globalData.userInfo || {}).id || "";
     params.data["teamId"] = that.teamId;
     params.data["teamRole"] = that.teamRole;
-    // console.log("main= " + that.host2 + params.url);
     wx.request({
       url: url,
       // url: that.host + params.url,

d48edd5a1bddf51991c2b2e9573c69cb598e1a59
WEID <chen@weid.ink>
2020-07-30 12:01:17 +0800

    feat: 人岗匹配页，调优避免切换tab页频繁刷新

--- station/station.js
+++ station/station.js
@@ -3,63 +3,6 @@ const app = getApp();
 Page({
   data:{
     checkedId: "",
-    menu: [
-      {
-        id: "GLL",
-        name:"管理类",
-        active: false
-      },
-      {
-        id: "JSG",
-        name: "技术岗",
-        active: true
-      },
-      {
-        id: "GPS",
-        name: "管培生",
-        active: false
-      },
-      {
-        id: "JYPX",
-        name: "教育培训",
-        active: false
-      },
-      {
-        id: "SC",
-        name: "市场",
-        active: false
-      },
-      {
-        id: "YY",
-        name: "运营",
-        active: false
-      },
-      {
-        id: "ZZ",
-        name: "咨询",
-        active: false
-      },
-      {
-        id: "JR",
-        name:"金融",
-        active: false
-      },
-      {
-        id: "LS",
-        name: "零售",
-        active: false
-      },
-      {
-        id: "CP",
-        name:"产品",
-        active: false
-      },
-      {
-        id: "XS/KF",
-        name:"销售/客服",
-        active: false
-      },
-    ],
     childs: [],
     statusbarHeight: app.globalData.statusbarHeight,
     titleHeight: app.globalData.titleHeight,
@@ -70,12 +13,6 @@ Page({
     wechat: "haola72",
   },
   onLoad: function(){
-
-  },
-  onShow: function(){
-    this.title = this.selectComponent("#title");
-    app.getUserInfo(this.title.loadUserMsg.call(this.title._this()));
-
     let that = this;
     app.doAjax({
       url: "positionTags",
@@ -96,6 +33,10 @@ Page({
         })
       }
     });
+  },
+  onShow: function(){
+    this.title = this.selectComponent("#title");
+    app.getUserInfo(this.title.loadUserMsg.call(this.title._this()));
   },
   changeTab: function (e) {
     const checkedId = e.currentTarget.id,

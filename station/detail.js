d9bd91b7f1ac5d1efcb6106eb6a789e614cdadcd
WEID <chen@weid.ink>
2020-09-02 17:53:26 +0800

    style: 修改button按钮颜色

--- station/detail.js
+++ station/detail.js
@@ -1,7 +1,7 @@
 Page({
     data: {},
     onLoad: function (options) {
-        const { id } = options;
+        const { id="5efed573b1ef0200062a85f7" } = options;
         wx.reLaunch({
             url: `../pages/station/components/detail/detail?id=${id}`,
         })

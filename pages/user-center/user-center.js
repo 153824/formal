e80036dc67f468abe158354beed3dd9e5e68c407
WEID <chen@weid.ink>
2020-09-02 14:45:39 +0800

    feat: 个人中心页添加企业微信相关变量

--- pages/user-center/user-center.js
+++ pages/user-center/user-center.js
@@ -1,8 +1,14 @@
+const app = getApp();
 Page({
     data: {
         active: 1,
+        isWxWork: app.wxWorkInfo.isWxWork,
+        isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
     },
     onLoad: function (options) {
-
+        this.setData({
+            isWxWork: app.wxWorkInfo.isWxWork,
+            isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
+        });
     }
 });

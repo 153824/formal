6ff6c2befbcf76b07a3fe9ae978275fc8368da2d
WEID <chen@weid.ink>
2020-09-10 19:15:22 +0800

    feat: 工作台适配企业端与微信小程序端

--- pages/work-base/work-base.js
+++ pages/work-base/work-base.js
@@ -1,3 +1,17 @@
+/***********************************************************************************************************************
+ * @NAME: WEID       /       @DATE: 2020/7/21      /       @DESC: 变量注释模板(新增变量务必添加)
+ * shareId: 领取测评ID
+ * active: tabbar激活ID
+ * isLogin: 是否登录
+ * loading: 是否加载中
+ * maskTrigger: 遮罩控制器
+ * isWxWork: 是否为企业微信
+ * isWxWorkAdmin: 是否为企业微信管理员
+ * myEvaluation: 我的测评列表
+ * evaluationTrack: 测评跟踪列表
+ * ********************************************************************************************************************/
+import {timeFormat} from "../../utils/utils";
+
 const app = getApp();
 Page({
     data: {
@@ -6,43 +20,92 @@ Page({
         isLogin: false,
         loading: true,
         maskTrigger: false,
-        reportListTrigger: false,
+        titleHeight: app.globalData.titleHeight,
+        statusbarHeight: app.globalData.statusbarHeight,
+        windowHeight: app.globalData.windowHeight,
+        pixelRate: app.globalData.pixelRate,
+        phoneModel: app.isIphoneX,
         isWxWork: app.wxWorkInfo.isWxWork,
-        isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin
+        isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
+        myEvaluation: [],
+        evaluationTrack: [],
+        reportsList: []
     },
 
-    onLoad: function(option={id:""}) {
-        const { isWxWorkAdmin,isWxWork } = this;
-        if( option.id ){
-            app.shareId = option.id;
+    onLoad: function (option = {id: ""}) {
+        const that = this;
+        const {isWxWorkAdmin, isWxWork} = this.data;
+        if (isWxWork && !isWxWorkAdmin) {
+            if (option.id) {
+                app.shareId = option.id;
+            }
+            const userInfo = wx.getStorageSync("userInfo");
+            if (userInfo && userInfo.avatar) {
+                this.setData({
+                    isLogin: true
+                });
+                return;
+            }
+            if (app.shareId) {
+                wx.reLaunch({
+                    url: "./components/guide/guide?id=" + app.shareId
+                });
+                app.shareId = null;
+                return;
+            }
         }
-        if( option.maskTrigger ){
+        if (!isWxWork) {
+            app.doAjax({
+                url: 'inventories',
+                method: 'get',
+                success: function (res) {
+                    that.setData({
+                        myEvaluation: res
+                    });
+                }
+            });
+            app.doAjax({
+                url: 'release_records',
+                method: 'get',
+                data: {
+                    isEE: true,
+                    page: 1,
+                    pageSize: 4,
+                },
+                success: function (res) {
+                    console.log(timeFormat);
+                    for (let i = 0; i < res.length; i++) {
+                        res[i].createdAt = timeFormat(res[i].createdAt);
+                    }
+                    console.log(res);
+                    that.setData({
+                        evaluationTrack: res
+                    });
+                }
+            });
+            app.doAjax({
+                url: `reports`,
+                method: "get",
+                data: {
+                    isEE: true,
+                    page: 1,
+                    pageSize: 3
+                },
+                success: function (res) {
+                    that.setData({
+                        reportsList: res
+                    })
+                }
+            })
+        }
+        if (option.maskTrigger) {
             this.setData({
                 maskTrigger: true
             })
         }
-        if ( app.shareId && this.data.isWxWork ) { //跳转到答题界面
-            wx.reLaunch({
-                url: "./components/guide/guide?id=" + app.shareId
-            });
-            app.shareId = null;
-            return;
-        }
-        const userInfo = wx.getStorageSync("userInfo");
-        if (userInfo && userInfo.avatar) {
-            this.setData({
-                isLogin: true
-            });
-            if(isWxWork && !isWxWorkAdmin){
-                this.setData({
-                    reportListTrigger: true,
-                });
-            }
-            return;
-        }
     },
 
-    getUserInfo: function(e) {
+    getUserInfo: function (e) {
         var that = this;
         var userData = e.detail.userInfo;
         if (!userData) {
@@ -67,7 +130,7 @@ Page({
                     }
                 })
             },
-            success: function(res) {
+            success: function (res) {
                 // console.log("updateUserMsg",res);
                 var userData = res.data;
                 if (0 == res.code) {
@@ -81,7 +144,7 @@ Page({
         });
     },
 
-    changePage: function(e) {
+    changePage: function (e) {
         var that = this;
         app.isTest = false;
         if (app.shareId) { //跳转到答题界面
@@ -91,17 +154,50 @@ Page({
             app.shareId = null;
         } else {
             wx.reLaunch({
-                url: "./components/report-list/report-list"
+                url: "./components/member-report-list/member-report-list"
             });
         }
     },
     /**
      * 进入测评模拟测试
      */
-    toTestIt: function(e) {
+    toTestIt: function (e) {
         app.isTest = true;
         wx.navigateTo({
             url: './components/guide/guide'
         });
+    },
+
+    goToTrackMore: function (e) {
+        wx.navigateTo({
+            url: "./components/track-more/track-more"
+        })
+    },
+
+    goToTrackDetail: function (e) {
+        const { trackId,trackIndex } = e.currentTarget.dataset;
+        const trackInfo = JSON.stringify(this.data.evaluationTrack[trackIndex]);
+        wx.navigateTo({
+            url: `./components/track-detail/track-detail?trackId=${trackId}&trackInfo=${trackInfo}`,
+        })
+    },
+
+    goToSharePaper: function (e) {
+        wx.navigateTo({
+            url: `../station/components/sharePaper/sharePaper`,
+        })
+    },
+
+    goToEvaluationDetail: function (e) {
+        const { evaluationId } = e.currentTarget.dataset;
+        wx.navigateTo({
+            url: `../station/components/detail/detail?id=${evaluationId}`,
+        })
+    },
+
+    goToReportMore: function (e) {
+        wx.navigateTo({
+            url: `./components/report-more/report-more`,
+        })
     }
 });

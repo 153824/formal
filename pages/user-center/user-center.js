c060b1e5ac23f63b5c64089eb6ac5a803a7ea6d2
WEID <chen@weid.ink>
2020-09-10 19:18:14 +0800

    feat: 用户中心适配企业端与微信小程序端

--- pages/user-center/user-center.js
+++ pages/user-center/user-center.js
@@ -1,14 +1,27 @@
 const app = getApp();
 Page({
     data: {
-        active: 1,
         isWxWork: app.wxWorkInfo.isWxWork,
         isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
     },
     onLoad: function (options) {
-        this.setData({
-            isWxWork: app.wxWorkInfo.isWxWork,
-            isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
-        });
+        const {isWxWork, isWxWorkAdmin} = this.data;
+        if (!isWxWork) {
+
+        } else if (isWxWork && isWxWorkAdmin) {
+
+        } else if (isWxWork && !isWxWorkAdmin) {
+
+        }
+    },
+    goToReceiveReports: function () {
+        wx.navigateTo({
+            url: './components/receive-reports/receive-reports'
+        })
+    },
+    goToReceiveEvaluation: function () {
+        wx.navigateTo({
+            url: './components/receive-evaluations/receive-evaluations'
+        })
     }
 });

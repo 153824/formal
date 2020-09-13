a659ce742f778c99ab42fff86e1f63788d7778d3
WEID <chen@weid.ink>
2020-09-13 15:50:33 +0800

    feat: 按组件的类型返回不同的测评报告

--- common/evaluation-reports/evaluation-reports.js
+++ common/evaluation-reports/evaluation-reports.js
@@ -4,27 +4,54 @@ Component({
         reportList: {
             type: Array,
             value: []
+        },
+        type: {
+            type: String,
+            value: "report-more" //receive-evaluation
         }
     },
     data: {
         reportList: []
     },
-    methods: {},
-    pageLifetimes: {
-        onLoad: function (options) {
-            const that = this;
-            app.doAjax({
-                url: `reports`,
-                method: "get",
-                data: {
-                    isEE: app.wxWorkInfo.isWxWork,
-                },
-                success: function (res) {
-                    that.setData({
-                        reportList: res
-                    })
-                }
+    methods: {
+        goToReport: function (e) {
+            const {receiveRecordId} = e.currentTarget.dataset;
+            wx.navigateTo({
+                url: `/pages/report/report?receiveRecordId=${receiveRecordId}`
             })
+        }
+    },
+    pageLifetimes: {
+        onLoad: function (options) {}
+    },
+    lifetimes: {
+        attached() {
+            const that = this;
+            const {type} = this.properties;
+            if (type === "report-more") {
+                app.doAjax({
+                    url: `reports`,
+                    method: "get",
+                    data: {
+                        isEE: app.wxWorkInfo.isWxWork,
+                    },
+                    success: function (res) {
+                        that.setData({
+                            reportList: res
+                        })
+                    }
+                })
+            } else if (type === "receive-evaluation") {
+                app.doAjax({
+                    url: `reports/accepted_list`,
+                    method: "get",
+                    success: function (res) {
+                        that.setData({
+                            reportList: res
+                        })
+                    }
+                })
+            }
         }
     }
 });

ca702a5f1dd044a55146edfd63cb94b732c61f90
WEID <chen@weid.ink>
2020-09-13 15:54:04 +0800

    feat: 匹配兼容微信小程序、企业微信小程序的他人分享给我的测评

--- pages/user-center/components/receive-evaluations/receive-evaluations.js
+++ pages/user-center/components/receive-evaluations/receive-evaluations.js
@@ -6,13 +6,36 @@ Page({
     onLoad: function (options) {
         const that = this;
         app.doAjax({
-            url: 'inventories',
+            url: 'receive_records',
             method: 'get',
             success: function (res) {
                 that.setData({
-                    myEvaluation: res
+                    evaluationTask: res
                 });
             }
         });
+    },
+    onShow() {
+        app.doAjax({
+            url: 'receive_records',
+            method: 'get',
+            success: function (res) {
+                that.setData({
+                    evaluationTask: res
+                });
+            }
+        });
+    },
+    goToAnswering: function (e) {
+        const {evaluationTask} = this.data;
+        const {receiveRecordId, evaluationId, evaluationIndex} = e.currentTarget.dataset;
+        console.log(evaluationId);
+        if (evaluationTask[evaluationIndex].status.toLowerCase() === 'finished') {
+            app.toast("已完成！");
+            return;
+        }
+        wx.navigateTo({
+            url: `/pages/work-base/components/answering/answering?receiveRecordId=${receiveRecordId}&pid=${evaluationId}`
+        })
     }
 });

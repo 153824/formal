1d3b48e10878ed2aa7087c4fab08b576dad7ad78
WEID <chen@weid.ink>
2020-09-13 19:46:05 +0800

    docs: 删除无用文件

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,1 @@
-const moment = require('./moment');
 
-function timeFormat(time) {
-    moment.locale('en', {
-        longDateFormat: {
-            l: "YYYY-MM-DD",
-            L: "YYYY-MM-DD HH:mm"
-        }
-    });
-    return moment(time).format('L');
-}
-
-export {
-    timeFormat,
-}

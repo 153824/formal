46342b739ce21d5fe14fd48a4e6e3153b53ef6a0
WEID <chen@weid.ink>
2020-09-18 11:04:52 +0800

    feat: 修改获取作答跟踪接口参数

--- pages/work-base/components/track-more/track-more.js
+++ pages/work-base/components/track-more/track-more.js
@@ -1,30 +1,19 @@
 const app = getApp();
 Page({
-    data: {
-        moreEvaluationTrack: []
-    },
+    data: {},
     onLoad: function (options) {
         const that = this;
         app.doAjax({
             url: 'release_records',
             method: 'get',
             data: {
-                isEE: true,
-                page: 1,
-                pageSize: 4,
+                isEE: app.wxWorkInfo.isWxWork,
             },
             success: function (res) {
                 that.setData({
-                    moreEvaluationTrack: res
+                    evaluationTrack: res
                 });
             }
         })
     },
-    goToTrackDetail: function (e) {
-        const { trackId,trackIndex } = e.currentTarget.dataset;
-        const trackInfo = JSON.stringify(this.data.moreEvaluationTrack[trackIndex]);
-        wx.navigateTo({
-            url: `/pages/work-base/components/track-detail/track-detail?trackId=${trackId}&trackInfo=${trackInfo}`,
-        })
-    }
 });

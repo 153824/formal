215a1ba32e264159775480ab449a38848bbd8096
WEID <chen@weid.ink>
2020-09-22 19:52:22 +0800

    fix: #CSXE-168 联系客服点击以后是原来的那个弹窗，不需要再进入新页面

--- pages/user-center/user-center.js
+++ pages/user-center/user-center.js
@@ -3,13 +3,13 @@ Page({
     data: {
         isWxWork: app.wxWorkInfo.isWxWork,
         isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
-        userInfo: wx.getStorageSync("userInfo") || app.globalData.userInfo || app.globalData.userMsg
+        userInfo: wx.getStorageSync("userInfo") || wx.getStorageSync("USER_DETAIL") || app.globalData.userInfo || app.globalData.userMsg
     },
     onLoad: function (options) {
         const {isWxWork, isWxWorkAdmin} = this.data;
         this.setData({
             isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
-            userInfo: wx.getStorageSync("userInfo") || app.globalData.userInfo || app.globalData.userMsg
+            userInfo: wx.getStorageSync("userInfo") || wx.getStorageSync("USER_DETAIL") || app.globalData.userInfo || app.globalData.userMsg
         });
         if (!isWxWork) {
             console.log("this.data.userInfo: ",this.data.userInfo)
@@ -23,7 +23,7 @@ Page({
     onShow() {
         console.log("this.data.userInfo: ",this.data.userInfo)
         this.setData({
-            userInfo:  wx.getStorageSync("userInfo") || app.globalData.userInfo || app.globalData.userMsg,
+            userInfo:  wx.getStorageSync("userInfo") || wx.getStorageSync("USER_DETAIL") || app.globalData.userInfo || app.globalData.userMsg,
             isWxWork: app.wxWorkInfo.isWxWork,
             isWxWorkAdmin: app.wxWorkInfo.isWxWorkAdmin,
         })
@@ -47,5 +47,32 @@ Page({
         wx.navigateTo({
             url: "/pages/user-center/components/serving-client/serving-client"
         })
-    }
+    },
+    getUserInfo: function(e) {
+        var that = this;
+        var userInfo = e.detail.userInfo;
+        console.log("userInfo: ",userInfo);
+        if (!userInfo) {
+            console.error("获取用户资料失败", e);
+            return;
+        }
+        userInfo["openid"] = wx.getStorageSync("openId") || app.globalData.userMsg.openid;
+        app.doAjax({
+            url: "updateUserMsg",
+            method: "post",
+            data: {
+                data: JSON.stringify({
+                    wxUserInfo: userInfo,
+                    userCompany: {
+                        name: userInfo.nickName + "的团队"
+                    }
+                }),
+            },
+            success: function(res) {
+                that.hideLoginDlg();
+                app.globalData.userInfo.nickname = userInfo.nickName;
+                app.addNewTeam(that.onShow);
+            }
+        });
+    },
 });

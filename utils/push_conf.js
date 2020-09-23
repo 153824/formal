b8fad5a36e4ba893425aeb7300587514c9fa9136
WEID <chen@weid.ink>
2020-09-23 18:05:52 +0800

    style: 修改与配置使用阿拉丁

--- utils/push_conf.js
+++ utils/push_conf.js
@@ -1,3 +1,3 @@
-exports.plugin = true; //是否引用插件
+exports.plugin = false; //是否引用插件
 exports.debug = false; //调试模式，开启后所有错误信息将以model形式提示给您
 exports.app_key = "88892812ce5fded4b697ca49fc6874cf";//小神推后台获取的app_key

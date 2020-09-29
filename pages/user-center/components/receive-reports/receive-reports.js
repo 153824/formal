b54ead7b9651a013277bc4d9a17dd73daafbb0eb
WEID <chen@weid.ink>
2020-09-29 13:47:11 +0800

    fix: 获取他人分享给我的报告发起多次请求

--- pages/user-center/components/receive-reports/receive-reports.js
+++ pages/user-center/components/receive-reports/receive-reports.js
@@ -8,27 +8,6 @@ Page({
         const that = this;
         const {shareAt,releaseRecordId,tabIndex} = options;
         app.doAjax({
-           url: 'reports/accepted_list',
-           method: 'get',
-           success: function (res) {
-               this.setData({
-                   moreReportsList: res
-               })
-           }
-        });
-        // if(shareAt&&releaseRecordId){
-        //     app.doAjax({
-        //         url: 'release_records/accept',
-        //         method: 'post',
-        //         data: {
-        //             sharedAt: shareAt,
-        //             releaseRecordId: releaseRecordId
-        //         }
-        //     })
-        // }else{
-        //
-        // }
-        app.doAjax({
             url: "release_records/accepted_list",
             method: 'get',
             success: function(res) {

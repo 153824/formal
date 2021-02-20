dd075cb2292a5f5bc6b95f8e25cc31776defa3f7
WEID <chen@weid.ink>
2021-02-20 15:03:10 +0800

    feat: 对接获取auth_code接口

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -3,34 +3,6 @@ Page({
     data: {},
     onLoad: function (options) {},
     wxAuthLogin(e) {
-        const that = this;
-        app.updateUserMobileByWeWork(e).then(res => {
-            return that.wxAuthUserInfo()
-        }).then(res => {
-            return that.getUserAuth(res)
-        }).then(res => {
-            wx.switchTab({
-                url: "/pages/work-base/work-base"
-            });
-        }).catch(err => {
-            console.error(err);
-            app.toast('授权手机号失败')
-        })
-    },
-    wxAuthUserInfo() {
-        const userInfoPromise = new Promise((resolve, reject) => {
-            wx.getUserInfo({
-                success: result => {
-                    resolve(result)
-                },
-                fail: err => {
-                    reject(err)
-                }
-            })
-        });
-        return userInfoPromise;
-    },
-    getUserAuth(e) {
-        return app.getUserAuth(e)
+        app.updateUserMobileByWeWork(e)
     }
 });

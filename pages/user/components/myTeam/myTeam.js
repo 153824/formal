712e01c9637636f912eb4eb16f769ec220f94b00
WEID <weid.chxx@gmail.com>
2021-05-10 15:25:32 +0800

    refactor: 调整加入团队接口

--- pages/user/components/myTeam/myTeam.js
+++ pages/user/components/myTeam/myTeam.js
@@ -66,13 +66,10 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function(e) {
-    var role = e.target.dataset.role;
-    var shareKey = `teamMsg_${wx.getStorageSync('userInfo').teamId}_${role}`;
-    console.log('shareKey: ',shareKey);
-    console.log("pages/user/components/teamInvite/teamInvite?key=" + shareKey);
+    const teamId = wx.getStorageSync('userInfo') ? wx.getStorageSync('userInfo').teamId : '';
     return {
       title: "邀请加入团队",
-      path: "pages/user/components/teamInvite/teamInvite?key=" + shareKey,
+      path: `pages/user/components/teamInvite/teamInvite?inviteId=${teamId}`,
       imageUrl: "http://ihola.luoke101.com/wxShareImg.png"
     };
   },
@@ -85,54 +82,17 @@ Page({
     var num = d.num;
     if (n && num > 0) {
       app.toast("当前会员版本仅支持" + num + "位" + n + "，请升级或删除现有" + n);
-      return;
     }
-    var role = +d.role || 1;
-    app.doAjax({
-      url: "getTeamInvite",
-      data: {
-        id: app.teamId,
-        role: role
-      },
-      success: function(res) {}
-    });
   },
+
   /**
-   * 移除子管理员权限
+   * 移除子管理员
    */
-  delMember: function(e) {
-    var that = this;
-    var index = e.currentTarget.dataset.index;
-    var obj = that.data.members[index];
-    wx.showModal({
-      title: '取消子管理员权限',
-      content: '确定将该成员降为普通成员吗',
-      success: function(ret) {
-        if (ret.confirm) {
-          app.doAjax({
-            url: "updateTeamMember",
-            data: {
-              id: app.teamId,
-              type: 2,
-              role: 1,
-              teamUserId: obj.id
-            },
-            success: function() {
-              app.toast("操作成功");
-              that.onShow();
-            }
-          });
-        }
-      }
-    });
-  },
-  /**
-   * 编辑子管理员
-   */
-  showEditMenu1: function(e) {
+  remove(e) {
     const that = this;
     const index = e.currentTarget.dataset.index;
     const obj = that.data.members[index];
+    console.log(obj);
     wx.showModal({
       title: "移除成员",
       content: "确定将该成员从团队中移除吗?",
@@ -144,8 +104,11 @@ Page({
             teamUserId: obj.id
           };
           app.doAjax({
-            url: "updateTeamMember",
-            data: postData,
+            url: "../wework/collaborators",
+            method: 'DELETE',
+            data: {
+              userId: obj.id
+            },
             success: function() {
               app.toast("操作成功");
               that.onShow();
@@ -155,57 +118,7 @@ Page({
       }
     });
   },
-  /**
-   * 编辑普通成员
-   */
-  showEditMenu: function(e) {
-    var that = this;
-    var index = e.currentTarget.dataset.index;
-    var obj = that.data.member[index];
-    wx.showActionSheet({
-      itemList: ['移除成员', '升级为子管理员'],
-      success(res) {
-        var tapIndex = res.tapIndex;
-        var t = "移除成员";
-        var txt = "确定将该成员从团队中移除吗";
-        var postData = {
-          id: app.teamId,
-          type: 3,
-          teamUserId: obj.objectId
-        };
-        if (tapIndex == 1) {
-          //升级为子管理员
-          t = "升级为子管理员";
-          txt = "确定将该成员升级为子管理员吗";
-          postData = {
-            id: app.teamId,
-            type: 2,
-            role: 2,
-            teamUserId: obj.objectId
-          };
-        }
-        wx.showModal({
-          title: t,
-          content: txt,
-          success: function(ret) {
-            if (ret.confirm) {
-              app.doAjax({
-                url: "updateTeamMember",
-                data: postData,
-                success: function() {
-                  app.toast("操作成功");
-                  that.onShow();
-                }
-              });
-            }
-          }
-        });
-      },
-      fail(res) {
-        console.error(res.errMsg)
-      }
-    });
-  },
+
   /**
    * 转让超级管理员
    */
@@ -216,6 +129,8 @@ Page({
       url: '../selTeamAdmin/selTeamAdmin'
     });
   },
+
+
   loadAdminInfo() {
     const that = this;
     app.doAjax({

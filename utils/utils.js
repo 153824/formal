7b85ca9e2fe0b70b206e9fd4bc67b6713e85fe6e
WEID <weid.chxx@gmail.com>
2021-05-26 16:05:10 +0800

    fix: 修复报告年龄显示异常

--- utils/utils.js
+++ utils/utils.js
@@ -19,3 +19,13 @@ export function isComplete(text, targetLen) {
     return text.length === targetLen
 }
 
+export function getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m <= 0) {
+        age--;
+    }
+    return age;
+}

7af3c1ab6a9f5efe7131a683d0f8aec89a4d41f3
WEID <weid.chxx@gmail.com>
2021-06-03 16:28:54 +0800

    feat: 优化MBTI报告模板

--- pages/report/components/personality/components/interpret/interpret.js
+++ pages/report/components/personality/components/interpret/interpret.js
@@ -28,13 +28,11 @@ Component({
             query.selectAll(`#detail-${index}`).fields({
                 size: true
             }).exec(function (res) {
-                console.log(res[0]);
                 let lineHeight = 24 // 固定高度值 单位：PX
                 for (var i = 0; i < res[0].length; i++) {
                     that.setData({
-                        canIUnfold: (res[0][i].height / lineHeight) > 3
+                        canIUnfold: (res[0][i].height / lineHeight) > 3.5
                     })
-                    console.log((res[0][i].height / lineHeight) > 3)
                 }
             })
         }

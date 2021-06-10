e299a5788cc0dc5a4978176e0695b4716fa35db6
WEID <weid.chxx@gmail.com>
2021-06-10 15:47:59 +0800

    fix: 日期默认开始为1900-01-01、去除调试代码块

--- pages/report/components/personality/personality.js
+++ pages/report/components/personality/personality.js
@@ -12,38 +12,6 @@ Component({
         }
     },
     data: {},
-    methods: {
-        init(option) {
-            const {labelColor, labelFontSize, percent, label} = option;
-            const ctx = wx.createCanvasContext('mbti', this);
-            // 绘制百分比
-            ctx.setFontSize(`${percent.fontWeight} ${percent.fontSize}`);
-            ctx.setFillStyle(percent.color);
-            ctx.fillText('81%', 10, 20);
-            // 绘制百分比对应的文本
-            ctx.setFontSize(`${label.fontWeight} ${label.fontSize}`);
-            ctx.setFillStyle(label.color);
-            ctx.fillText('外向型', 6, 35);
-
-            // ctx.fillRect(10, 10, 150, 75);
-            ctx.draw();
-        },
-    },
-    lifetimes: {
-        ready() {
-            const option = {
-                percent: {
-                    color: '#353EE8',
-                    fontSize: +(30 * app.rate).toFixed(0),
-                    fontWeight: 600
-                },
-                label: {
-                    color: '#353EE8',
-                    fontSize: +(26 * app.rate).toFixed(0),
-                    fontWeight: 600
-                },
-            };
-            this.init(option);
-        }
-    }
+    methods: {},
+    lifetimes: {}
 });

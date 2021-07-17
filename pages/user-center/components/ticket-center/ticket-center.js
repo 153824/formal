015db24058d7b3752621060eae6867d272f88241
WEID <weid.chxx@gmail.com>
2021-07-17 17:39:23 +0800

    feat: 对接用户券接口

--- pages/user-center/components/ticket-center/ticket-center.js
+++ pages/user-center/components/ticket-center/ticket-center.js
@@ -1,7 +1,39 @@
+const app = getApp();
 Page({
-    data: {},
-    onLoad: function (options) {
-
+    data: {
+        page: 0,
+        tickets: [],
+        isEmpty: true,
     },
+    onLoad: function (options) {
+        this.loadTickets();
+    },
+    loadTickets() {
+        const that = this;
+        const {tickets, page} = this.data;
+        app.doAjax({
+            url: 'wework/vouchers',
+            method: 'GET',
+            data: {
+                page: page + 1,
+                size: 10
+            },
+            success(res) {
+                const {empty, page} = res;
+                const targetTickets = [...tickets, ...res.items]
+                console.log(targetTickets);
+                that.setData({
+                    page,
+                    tickets: targetTickets,
+                    isEmpty: empty
+                });
+            },
+            fail() {
 
+            }
+        })
+    },
+    goToNext() {
+        this.loadTickets();
+    }
 });

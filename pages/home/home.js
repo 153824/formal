976e2b23e9aff709ebbaabd3f57f56e3f96a57dc
WEID <weid.chxx@gmail.com>
2021-08-07 11:56:31 +0800

    feat: 修复tabbar显示异常

--- pages/home/home.js
+++ pages/home/home.js
@@ -26,7 +26,8 @@ Page({
         },
         bannerRes: [],
         isGetAccessToken: app.checkAccessToken(),
-        authCodeCounter: 0
+        authCodeCounter: 0,
+        isShowTabbar: false
     },
 
     onLoad: function () {
@@ -75,6 +76,7 @@ Page({
     },
 
     init() {
+        const that = this;
         if(app.checkAccessToken()){
             const {isWxWork} = app.wxWorkInfo;
             const {is3rd} = app.wx3rdInfo;
@@ -87,10 +89,19 @@ Page({
                 isWxWorkSuperAdmin
             })
             this.setData({
-                isGetAccessToken: true
+                isGetAccessToken: true,
+                isWxWork,
+                isWxWorkAdmin,
+                is3rd,
+                isWxWorkSuperAdmin
+            }, ()=>{
+                that.setData({
+                    isShowTabbar: true
+                })
             })
         } else {
             app.checkUserInfo=(res)=>{
+                console.log(res)
                 this.check({
                     isWxWork: res.isWxWork,
                     isWxWorkAdmin: res.isAdmin,
@@ -98,17 +109,27 @@ Page({
                     isWxWorkSuperAdmin: res.isSuperAdmin
                 })
                 this.setData({
-                    isGetAccessToken: res.tokenInfo && res.tokenInfo.accessToken
+                    isGetAccessToken: res.tokenInfo && res.tokenInfo.accessToken,
+                    isWxWork: res.isWxWork,
+                    isWxWorkAdmin: res.isAdmin,
+                    is3rd: res.is3rd,
+                    isWxWorkSuperAdmin: res.isSuperAdmin
+                }, ()=>{
+                    that.setData({
+                        isShowTabbar: true
+                    })
                 })
             };
         }
     },
 
     check({isWxWork, isWxWorkAdmin, isWxWorkSuperAdmin, is3rd}) {
+        const that = this;
         if (is3rd || isWxWork && !isWxWorkSuperAdmin) {
             let flag = false
             let url = isWxWork ? "/pages/account/account" : "/pages/auth/auth?type=auth"
             if (app.checkAccessToken()) {
+                console.log('checkAccessToken')
                 url = '/pages/work-base/work-base'
                 wx.switchTab({
                     url: url
@@ -121,8 +142,20 @@ Page({
             return
         }
         if (!is3rd && !isWxWork || !is3rd && isWxWork && isWxWorkSuperAdmin) {
-            this.loadSection();
-            this.loadBanner();
+            this.loadSection()
+                .then(res=>{
+                    return this.loadBanner()
+                })
+                .then(res=>{
+                    that.setData({
+                        loading: false
+                    })
+                })
+                .catch(err=>{
+                    that.setData({
+                        loading: false
+                    })
+                })
         }
         const umaConfig = umaEvent.launchHome;
         wx.uma.trackEvent(umaConfig.tag, {env: getEnv(wx), tag: getTag(wx)});
@@ -191,59 +224,66 @@ Page({
         const {evaluationSection} = this.data;
         const evaluationList = evaluationSection.evaluations;
         const {page, size} = this.data;
-        app.doAjax({
-            url: '../wework/homepages/evaluations',
-            method: 'GET',
-            data: {
-                page: page + 1,
-                size,
-            },
-            success(res) {
-                const {evaluations, title} = res;
-                if(evaluations.length){
-                    const targetEvaluations = [...evaluationList, ...evaluations]
-                    evaluationSection.evaluations = targetEvaluations;
-                    evaluationSection.title = title
-                    console.log({...evaluationSection})
-                    that.setData({
-                        evaluationSection: {...evaluationSection},
-                        page: page + 1,
-                    })
-                } else {
-                    evaluationSection.title = title
-                    that.setData({
-                        evaluationSection: {...evaluationSection},
-                    })
-                    app.toast('已为您加载所有相关内容')
+        const p = new Promise((resolve, reject) => {
+            app.doAjax({
+                url: '../wework/homepages/evaluations',
+                method: 'GET',
+                data: {
+                    page: page + 1,
+                    size,
+                },
+                success(res) {
+                    const {evaluations, title} = res;
+                    if(evaluations.length){
+                        const targetEvaluations = [...evaluationList, ...evaluations]
+                        evaluationSection.evaluations = targetEvaluations;
+                        evaluationSection.title = title
+                        console.log({...evaluationSection})
+                        that.setData({
+                            evaluationSection: {...evaluationSection},
+                            page: page + 1,
+                        })
+                    } else {
+                        evaluationSection.title = title
+                        that.setData({
+                            evaluationSection: {...evaluationSection},
+                        })
+                        app.toast('已为您加载所有相关内容')
+                    }
+                    resolve(res);
+                },
+                error(err) {
+                    reject(err);
                 }
-                console.log(res);
-            },
-            error(err) {
-
-            }
-        })
+            });
+        });
+        return p;
     },
 
     loadBanner() {
         const that = this;
-        app.doAjax({
-            url: "homePages",
-            method: "get",
-            noLoading: true,
-            success(res) {
-                const targetData = [];
-                const {data} = res.resultObject.banner;
-                data.forEach(item=>{
-                    targetData.push(item)
-                })
-                that.setData({
-                    bannerRes: data
-                })
-            },
-            error(err) {
-
-            }
+        const p = new Promise((resolve, reject) => {
+            app.doAjax({
+                url: "homePages",
+                method: "get",
+                noLoading: true,
+                success(res) {
+                    const targetData = [];
+                    const {data} = res.resultObject.banner;
+                    data.forEach(item=>{
+                        targetData.push(item)
+                    })
+                    that.setData({
+                        bannerRes: data
+                    })
+                    resolve(res)
+                },
+                error(err) {
+                    reject(err)
+                }
+            })
         })
+        return p;
     },
 
     getPhoneNumber(e) {

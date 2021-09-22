c29c81055980e9b8b745511cb58855e473fa1712
WEID <weid.chxx@gmail.com>
2021-09-22 18:51:14 +0800

    feat: 新增购买记录

--- pages/station/components/usage-record/usage-record.js
+++ pages/station/components/usage-record/usage-record.js
@@ -1,6 +1,23 @@
 Page({
-    data: {},
+    data: {
+        evaluationId: ''
+    },
     onLoad: function (options) {
 
+    },
+    goToEvaluationDetail() {
+        const {evaluationId} = this.data;
+        wx.navigateTo({
+            url: `/pages/station/components/detail/detail?id=${evaluationId}`
+        });
+    },
+    goToGenerate() {
+        const necessaryInfo = {
+            evaluationId: '',
+            norms: '',
+        };
+        wx.navigateTo({
+            url: `/pages/station/components/generate/generate?necessaryInfo=${JSON.stringify(necessaryInfo)}`,
+        });
     }
 });

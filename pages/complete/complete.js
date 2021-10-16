db1aa120f07215d2daf2ee5fd90b95b9c9943441
WEID <weid.chxx@gmail.com>
2021-10-16 13:48:04 +0800

    feat: 新增完善信息页

---
+++ pages/complete/complete.js
@@ -0,0 +1,113 @@
+import debounce from "../../utils/lodash/debounce";
+
+const app = getApp();
+
+Page({
+    data: {
+        positionList: [
+            'HR专员(招聘/薪酬/培训/绩效)',
+            'HR主管/经理',
+            'HRBP',
+            'OD/TD/LD',
+            'HRD/HRM',
+            '业务部门负责人',
+            'CEO/老板',
+            '其他'
+        ],
+        positionIndex: -1,
+        companyModelList: [
+            '15人以下',
+            '15-50人',
+            '50-150人',
+            '150-500人',
+            '500-1000人',
+            '1000人以上'
+        ],
+        companyModelIndex: -1,
+        lastKeyword: '',
+        lastResult: [],
+        companyList: [],
+        selectCompanyTrigger: false,
+        currentCompany: '',
+        currentKeyword: ''
+    },
+
+    onLoad: function (options) {
+
+    },
+
+    onPositionChange(e) {
+        const {value} = e.detail;
+        this.setData({
+            positionIndex: value
+        })
+    },
+
+    onCompanyModelChange(e) {
+        console.log('onCompanyModelChange: ', e);
+        const {value} = e.detail;
+        this.setData({
+            companyModelIndex: value
+        })
+    },
+
+    loadCompany: debounce(function (e){
+        let canISearch = true;
+        const currentKeyword = e.detail.value;
+        const {lastResult, lastKeyword} = this.data;
+        if(currentKeyword.indexOf(lastKeyword)>-1){
+            if(lastResult.length === 0){
+                canISearch = false;
+            }
+        }
+        if(!lastKeyword && currentKeyword){
+            canISearch = true;
+        }
+        if(!currentKeyword){
+            canISearch = false;
+        }
+        canISearch && this.search(currentKeyword);
+    }, 500, {trailing: true, leading: false}),
+
+    search(currentKeyword) {
+        const that = this;
+        console.log('currentKeyword: ', currentKeyword);
+        app.doAjax({
+            url: '../wework/potential_customer/company',
+            method: 'GET',
+            noLoading: true,
+            data: {
+                name: currentKeyword,
+            },
+            success(res) {
+                that.setData({
+                    lastKeyword: currentKeyword,
+                    lastResult: res,
+                    companyList: res,
+                });
+                that.showSelectCompany();
+            }
+        });
+    },
+
+    showSelectCompany() {
+        this.setData({
+            selectCompanyTrigger: true
+        })
+    },
+
+    hideSelectCompany() {
+        this.setData({
+            selectCompanyTrigger: false
+        })
+    },
+
+    selectCompany(e) {
+        const {company} = e.currentTarget.dataset;
+        this.setData({
+            currentCompany: company,
+            currentKeyword: company,
+        });
+        this.hideSelectCompany();
+    },
+});
